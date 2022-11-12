c3366d9e310d836db86d9f40315ac008f3e04b6b
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-11-12 20:27:40 +0200

    Update and rename src/main/java/com/facebook/fbloginsample/callbacks/GetUserCallback.kt to src/main/java/com/facebook/liamslime/callbacks/GetUserCallback.kt

--- src/main/java/com/facebook/liamslime/callbacks/GetUserCallback.kt
+++ src/main/java/com/facebook/liamslime/callbacks/GetUserCallback.kt
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.fbloginsample.callbacks
+package com.facebook.liamslime.callbacks
 
 import android.net.Uri
 import com.facebook.GraphRequest
