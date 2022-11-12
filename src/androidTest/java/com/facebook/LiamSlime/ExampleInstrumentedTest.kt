a0b1fc6505e97b49a0380d041e6fb9b900f78412
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-11-12 20:15:05 +0200

    Update and rename src/androidTest/java/com/facebook/fbloginsample/ExampleInstrumentedTest.kt to src/androidTest/java/com/facebook/LiamSlime/ExampleInstrumentedTest.kt

--- src/androidTest/java/com/facebook/LiamSlime/ExampleInstrumentedTest.kt
+++ src/androidTest/java/com/facebook/LiamSlime/ExampleInstrumentedTest.kt
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.fbloginsample
+package com.facebook.liamslime
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
