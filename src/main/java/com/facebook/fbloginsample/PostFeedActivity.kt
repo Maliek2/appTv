8bb8e85ff0a021ace12ebbf114017abbbd9be6cf
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-11-12 20:32:35 +0200

    Update PostFeedActivity.kt

--- src/main/java/com/facebook/fbloginsample/PostFeedActivity.kt
+++ src/main/java/com/facebook/fbloginsample/PostFeedActivity.kt
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.fbloginsample
+package com.facebook.liamslime
 
 import android.app.Activity
 import android.content.Intent
