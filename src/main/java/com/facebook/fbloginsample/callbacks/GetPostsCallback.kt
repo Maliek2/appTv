8c74537ce4ba3fc72a636a25530d3cbb0c98a19a
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-11-12 20:26:48 +0200

    Update GetPostsCallback.kt

--- src/main/java/com/facebook/fbloginsample/callbacks/GetPostsCallback.kt
+++ src/main/java/com/facebook/fbloginsample/callbacks/GetPostsCallback.kt
@@ -5,7 +5,7 @@
  * This source code is licensed under the license found in the
  * LICENSE file in the root directory of this source tree.
  */
-package com.facebook.fbloginsample.callbacks
+package com.facebook.liamslime.callbacks
 
 import com.facebook.GraphRequest
 import com.facebook.fbloginsample.entities.Post
