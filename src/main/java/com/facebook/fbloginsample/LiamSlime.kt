2967d8b7c841e95cf575b56cfd8cb1407aba8bfc
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-11-12 20:11:50 +0200

    Update and rename FBLoginSampleApp.kt to LiamSlime.kt

--- src/main/java/com/facebook/fbloginsample/LiamSlime.kt
+++ src/main/java/com/facebook/fbloginsample/LiamSlime.kt
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.fbloginsample
+package com.facebook.liamslime
 
 import android.app.Application
 import com.facebook.drawee.backends.pipeline.Fresco
